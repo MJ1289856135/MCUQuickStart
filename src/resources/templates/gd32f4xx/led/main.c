682606cb2b68ffc5b354766c3689c44a4bfadfe1
MaJie <1289856135@qq.com>
2026-05-19 17:11:40 +0800

    fix: F4 uvprojx模板路径修正 + F4专用LED/UART模板（F4 GPIO API不同于F10x）
    - 修复uvprojx模板中所有 ..\..\Firmware\ 路径为正确的 ..\FIRMWARE\ 和 ..\CMSIS- F4 LED模板改用 gpio_mode_set + gpio_output_options_set（F4 API）
    - F4 UART模板改用 gpio_mode_set + gpio_af_set（F4 AF模式配置）
    - F4和F10x的GPIO API完全不同，模板需各自维护

--- src/resources/templates/gd32f4xx/led/main.c
+++ src/resources/templates/gd32f4xx/led/main.c
@@ -5,7 +5,8 @@
 void led_init(void)
 {
     rcu_periph_clock_enable(RCU_GPIOA);
-    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_0);
+    gpio_mode_set(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_0);
+    gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_0);
 }
 
 int main(void)

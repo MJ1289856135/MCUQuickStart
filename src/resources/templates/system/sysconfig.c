3185d3bed228517aa79c465e6f59ea4534a1846e
MaJie <1289856135@qq.com>
2026-05-22 15:08:47 +0800

    fix: sysconfig.c MSR_MSP改用编译器宏自动识别ARMCC/GCC/IAR

--- src/resources/templates/system/sysconfig.c
+++ src/resources/templates/system/sysconfig.c
@@ -19,8 +19,24 @@ void INTX_ENABLE(void)
 {
     __ASM volatile("cpsie i");
 }
-//设置栈顶地址（CMSIS内联汇编，GCC/ARMCC/IAR通用）
-void MSR_MSP(uint32_t addr)
+//设置栈顶地址
+//addr:栈顶地址
+#if defined(__CC_ARM) || defined(__ICCARM__) || defined(__ARMCC_VERSION)
+// ARMCC V5/V6, IAR
+__asm void MSR_MSP(uint32_t addr)
 {
-    __ASM volatile("MSR msp, %0" : : "r" (addr));
+    MSR MSP, r0
+    BX r14
 }
+#elif defined(__GNUC__)
+// GCC
+__attribute__((naked)) void MSR_MSP(uint32_t addr)
+{
+    __asm volatile (
+        "MSR MSP, r0\n\t"
+        "BX LR"
+    );
+}
+#else
+#error "Unsupported compiler"
+#endif
